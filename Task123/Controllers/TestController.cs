1245dcbe51a1ad70096ea87f3e1e4a5194a2a7bd
09092021 <09092021@DESKTOP-I8RNFBJ>
2021-10-19 16:33:14 +0400

    Add project files.

---
+++ Task123/Controllers/TestController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task123.Models;
+
+namespace Task123.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        ApplicationDbContext _context;
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string name)
+        {
+            User user = new User
+            {
+                FirstName = name,
+                Age = 18
+            };
+            _context.Add(user);
+            _context.SaveChanges();
+        }
+        
+        [HttpPut]
+        public void Put(int id,[FromBody] string name)
+        {
+            User user = _context.Users.Find(id);
+            user.FirstName = name;
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetNames()
+        {
+            return _context.Users.Select(x => x.FirstName).ToList();
+        }
+
+        [HttpGet("{id}")]
+        public string GetName(int id)
+        {
+            return _context.Users.Find(id).FirstName;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            User user = _context.Users.Find(id);
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Task123/Migrations/20211019122327_LastnameRemoved.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task123.Migrations
+{
+    public partial class LastnameRemoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
